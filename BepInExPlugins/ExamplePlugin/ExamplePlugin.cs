43909ce098273e7fdddcad8bce299b9b92edd7e3
Nicholas Toby <cheesasaurus@gmail.com>
2025-06-04 15:35:13 -0500

    set up ExamplePlugin project

---
+++ BepInExPlugins/ExamplePlugin/ExamplePlugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using VRisingMods.Core.Utilities;
+
+namespace ExamplePlugin;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("HookDOTS.VRisingBootstrapper")]
+public class ExamplePlugin : BasePlugin
+{
+    Harmony _harmony;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        LogUtil.Init(Log);
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+
+        // Harmony patching
+        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        _harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+    }
+
+    public override bool Unload()
+    {
+        _harmony?.UnpatchSelf();
+        return true;
+    }
+
+}
---
+++ BepInExPlugins/ExamplePlugin/README.md
@@ -0,0 +1,3 @@
+# ExamplePlugin
+
+⚠️WIP⚠️
