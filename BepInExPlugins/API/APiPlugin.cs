44fea09682bd48f12790bc770985b874844a63ca
Nicholas Toby <cheesasaurus@gmail.com>
2025-06-07 00:28:16 -0500

    Added core project

--- BepInExPlugins/API/APiPlugin.cs
+++ BepInExPlugins/API/APiPlugin.cs
@@ -29,6 +29,8 @@ public class ApiPlugin : BasePlugin
         // but this may change as features are added
         TypeManager.Initialize();
         Bus.Instance.TriggerGameReadyForHooking();
+
+        var x = Core.Hello.Hi; // todo: remove
     }
 
     public override bool Unload()
---
+++ Core/src/Hello.cs
@@ -0,0 +1,6 @@
+namespace HookDOTS.Core;
+
+public static class Hello
+{
+    public static string Hi = "hi!";
+}
